329d3214b32dd6470d4eeed3ac7f05df335a348b
Nicholas Esene <nickmerah@gmail.com>
2023-04-19 11:50:18 -0700

    second set of tasks

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,53 +2,30 @@
 #include <stdlib.h>
 #include "3-calc.h"
 /**
- * op_add - adds 5 functions
- * @a: int a
- * @b: int b
- * Return: sum of a and b
+ * get_op_func - gets the right func
+ * @s: function s
+ * Return: int value
  */
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
-/**
- * op_sub - subtracts
- * @a: int a
- * @b: int b
- * Return: difference
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-/**
- * op_mul - multiplies
- * @a: int a
- * @b: int b
- * Return: multipliy
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-/**
- * op_div - division
- * @a: int a
- * @b: int b
- * Return: division
- */
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-/**
- * op_mod - modulos
- * @a: int a
- * @b: int b
- * Return: modulo of ints
- */
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-int op_mod(int a, int b)
-{
-	return (a % b);
+	i = 0;
+	while (ops[i].f != NULL)
+	{
+		if (*s == *(ops[i].op) && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
+
+	printf("Error\n");
+	exit(99);
 }
